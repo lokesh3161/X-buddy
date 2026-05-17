c1d0151faab2f3de63078a6e42cf143e136c4bfc
lokesh3161 <lokesh3161@gmail.com>
2026-05-17 20:41:50 +0530

    Update Cloudflare tunnel URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_URL     = 'https://script.google.com/macros/s/AKfycbyj8UTgncnMmmz4ERZIN49PiHqPOS2GnBABOKgQ9WEirPh8aHSt0tdCcKkv2nUqeKt9/exec'
-const LOCAL_AGENT = 'https://person-linda-requested-antique.trycloudflare.com'
+const LOCAL_AGENT = 'https://precious-dietary-satisfactory-degrees.trycloudflare.com'
 
 // Send order text data to GAS → saves to Sheets instantly
 function gasGet(params) {
