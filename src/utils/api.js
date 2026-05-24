f49e48b3a1f151c10ead1441d7e441061f48f9bd
lokesh3161 <lokesh3161@gmail.com>
2026-05-24 19:14:01 +0530

    Auto-update Cloudflare tunnel URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_URL     = 'https://script.google.com/macros/s/AKfycbyj8UTgncnMmmz4ERZIN49PiHqPOS2GnBABOKgQ9WEirPh8aHSt0tdCcKkv2nUqeKt9/exec'
-const LOCAL_AGENT = ' https://chief-perry-drove-sims.trycloudflare.com'
+const LOCAL_AGENT = 'https://img-ruled-give-bandwidth.trycloudflare.com'
 
 function gasGet(params) {
   return fetch(`${API_URL}?${new URLSearchParams(params).toString()}`)
