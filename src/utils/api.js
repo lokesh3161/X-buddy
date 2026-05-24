f5210d2e7f18744abf76805f689d5185adfd463b
lokesh3161 <lokesh3161@gmail.com>
2026-05-24 17:08:47 +0530

    Auto-update Cloudflare tunnel URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_URL     = 'https://script.google.com/macros/s/AKfycbyj8UTgncnMmmz4ERZIN49PiHqPOS2GnBABOKgQ9WEirPh8aHSt0tdCcKkv2nUqeKt9/exec'
-const LOCAL_AGENT = ' https://irc-pays-projected-holmes.trycloudflare.com'
+const LOCAL_AGENT = ' https://derby-susan-south-olympics.trycloudflare.com'
 
 function gasGet(params) {
   return fetch(`${API_URL}?${new URLSearchParams(params).toString()}`)
