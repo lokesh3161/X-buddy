e7a6539f3c316dd8c3c4cf8696a885200c14d651
lokesh3161 <lokesh3161@gmail.com>
2026-05-24 16:29:22 +0530

    Auto-update Cloudflare tunnel URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_URL     = 'https://script.google.com/macros/s/AKfycbyj8UTgncnMmmz4ERZIN49PiHqPOS2GnBABOKgQ9WEirPh8aHSt0tdCcKkv2nUqeKt9/exec'
-const LOCAL_AGENT = 'https://jonathan-knew-medium-ken.trycloudflare.com'
+const LOCAL_AGENT = ' https://tub-checks-highs-objectives.trycloudflare.com'
 
 function gasGet(params) {
   return fetch(`${API_URL}?${new URLSearchParams(params).toString()}`)
