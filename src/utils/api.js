afb9b0ab1b806b2ecc1c922349dafff0fc034339
lokesh3161 <lokesh3161@gmail.com>
2026-05-25 07:32:19 +0530

    Auto-update Cloudflare tunnel URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_URL     = 'https://script.google.com/macros/s/AKfycbyj8UTgncnMmmz4ERZIN49PiHqPOS2GnBABOKgQ9WEirPh8aHSt0tdCcKkv2nUqeKt9/exec'
-const LOCAL_AGENT = ' https://arguments-oakland-passive-accessible.trycloudflare.com'
+const LOCAL_AGENT = 'https://clone-interior-memory-luxury.trycloudflare.com'
 
 function gasGet(params) {
   return fetch(`${API_URL}?${new URLSearchParams(params).toString()}`)
