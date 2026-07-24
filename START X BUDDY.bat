8f77ee7bb28535bace47361e39025df4ac01ed14
lokesh3161 <lokesh3161@gmail.com>
2026-07-24 16:17:03 +0530

    fix: update GAS URL to latest deployment

--- START X BUDDY.bat
+++ START X BUDDY.bat
@@ -8,7 +8,7 @@ set CLOUDFLARED=%AGENT_DIR%\cloudflared.exe
 set TUNNEL_LOG=%AGENT_DIR%\tunnel.log
 set TUNNEL_ERR=%AGENT_DIR%\tunnel_err.log
 set GIT=C:\Program Files\Git\bin\git.exe
-set GAS_URL=https://script.google.com/macros/s/AKfycbwVwHaLIhfCy79DNs66YWKp-eLehHMwKx_UEm-xz0xMPCoAsggn64xmjFXl0KtBNx3Q/exec
+set GAS_URL=https://script.google.com/macros/s/AKfycbyWiu74FuFA-m-uord17vVKSN67y3_Hr7gH1u-mZ6SHafeD818LvRaA194C517_HinS/exec
 
 echo.
 echo  X Buddy Master Startup
